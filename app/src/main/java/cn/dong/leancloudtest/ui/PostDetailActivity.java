a33ad7b8e601f1eabbc654ebd1268c04b7916878
dong <dongorigin@gmail.com>
2015-07-12 22:44:07 +0800

    解决fab阴影问题，完善应用style对Lollipop的支持

---
+++ app/src/main/java/cn/dong/leancloudtest/ui/PostDetailActivity.java
@@ -0,0 +1,14 @@
+package cn.dong.leancloudtest.ui;
+
+import cn.dong.leancloudtest.R;
+import cn.dong.leancloudtest.ui.common.BaseActivity;
+
+/**
+ * author DONG 2015/7/12.
+ */
+public class PostDetailActivity extends BaseActivity {
+    @Override
+    protected int getContentViewLayoutId() {
+        return R.layout.activity_post_detail;
+    }
+}
